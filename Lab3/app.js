13ea6d0cff0ce696e3e73fa605f42bf772f8298e
Braj Mohan Singh <shivasinghj8.2001@gmail.com>
2026-08-18 10:29:44 +0530

    Refactor server setup to enhance response styling and simplify request handling

--- Lab3/app.js
+++ Lab3/app.js
@@ -1,9 +1,15 @@
 import http from 'http';
-const server = http.createServer();
-server.on('request', (req, res) => {
-    res.write("<h1>Welcome to Server Side Programming</h1>");
+// const server = http.createServer();
+// server.on('request', (req, res) => {
+//     res.write("<h1>Welcome to Server Side Programming</h1>");
+//     res.end();
+// });
+
+const server = http.createServer((req, res) => {
+    res.write("<h1 style='color: red;'>Welcome to Server Side Programming</h1>");
     res.end();
 });
+
 server.listen(5001, () =>{
     console.log("Server is running on port 5001");
 })
